2bac5817db953e296e6116c35a12d28ebec625b8
Amir A. Shabani <amirshabani011@gmail.com>
2020-11-22 06:40:51 +0330

    Add days with zero tweets

--- tweets_rate_daily.py
+++ tweets_rate_daily.py
@@ -2,9 +2,12 @@ from datetime import datetime as dt
 from dateutil import parser
 from collections import Counter
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 import json
 
 days = []
+all_days = {}
 
 with open("tweet.js", 'r') as f:
 	text = f.read().split('=', 1)[1].lstrip()
@@ -14,14 +17,26 @@ for tweet in data:
 	date = parser.parse(tweet["tweet"]["created_at"], fuzzy=True)
 	days.append(date.strftime("%Y-%m-%d"))
 
-counter = list(Counter(days).items())
-day_freq = sorted(counter, key= lambda x: dt.strptime(x[0], "%Y-%m-%d"))
-x, y = zip(*day_freq)
+counter = Counter(days).items()
+counterl = list(counter)
+counterd = dict(counter)
+sorted_days = sorted(counterl, key= lambda x: dt.strptime(x[0], "%Y-%m-%d"))
+sdate = dt.strptime(sorted_days[0][0], "%Y-%m-%d")
+edate = dt.strptime(sorted_days[-1][0], "%Y-%m-%d")
+days_range = pd.date_range(sdate,edate, freq='d')
+months_range = pd.date_range(sdate, edate, freq='m')
+for day in list(pd.Series(days_range.format())):
+	if day in counterd:
+		all_days[day] = counterd[day]
+	else:
+		all_days[day] = 0
 
-plt.figure()
-plt.plot(list(x), list(y))
-plt.title("Tweets Rate - Daily")
-plt.xlabel("Days")
-plt.ylabel("Number of tweets")
-plt.xticks([], [])
-plt.savefig("plot.png", dpi=200)
+fig, ax = plt.subplots()
+ax.plot(list(all_days.keys()), list(all_days.values()))
+ax.set_title("Tweets Rate - Daily")
+ax.set_ylabel("Number of tweets")
+ax.set_xticks(list(pd.Series(months_range.format())))
+ax.tick_params(axis='x', labelrotation=90, labelsize=7)
+fig.tight_layout()
+fig.savefig("plot.png", dpi=200)
+
