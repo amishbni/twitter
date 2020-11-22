0349b63eae881a5ba46dbdcf3e95a1e15ede0937
Amir A. Shabani <amirshabani011@gmail.com>
2020-11-22 07:31:29 +0330

    Add first and last dates

--- tweets_rate_daily.py
+++ tweets_rate_daily.py
@@ -20,12 +20,19 @@ for tweet in data:
 counter = Counter(days).items()
 counterl = list(counter)
 counterd = dict(counter)
+
 sorted_days = sorted(counterl, key= lambda x: dt.strptime(x[0], "%Y-%m-%d"))
 sdate = dt.strptime(sorted_days[0][0], "%Y-%m-%d")
 edate = dt.strptime(sorted_days[-1][0], "%Y-%m-%d")
 days_range = pd.date_range(sdate,edate, freq='d')
+days_list = list(pd.Series(days_range.format()))
 months_range = pd.date_range(sdate, edate, freq='m')
-for day in list(pd.Series(days_range.format())):
+months_list = list(pd.Series(months_range.format()))
+if sdate not in months_list:
+	months_list.insert(0, sdate.strftime("%Y-%m-%d"))
+if edate not in months_list:
+	months_list.append(edate.strftime("%Y-%m-%d"))
+for day in days_list:
 	if day in counterd:
 		all_days[day] = counterd[day]
 	else:
@@ -36,7 +43,7 @@ fig, ax = plt.subplots()
 ax.plot(list(all_days.keys()), list(all_days.values()))
 ax.set_title("Tweets Rate - Daily")
 ax.set_ylabel("Number of tweets")
-ax.set_xticks(list(pd.Series(months_range.format())))
+ax.set_xticks(months_list)
 ax.tick_params(axis='x', labelrotation=90, labelsize=7)
 fig.tight_layout()
 fig.savefig("plot.png", dpi=200)
